06c9fc4a579e78cd8ae1bfc1121ab4509fd455b4
smrithinns <smrithinns@yahoo.co.in>
2019-02-17 08:17:36 +0530

    Almost

--- DuplDom.sh
+++ DuplDom.sh
@@ -3,7 +3,7 @@
 #Script to read list of domains in multiple cpanel server and find duplicates and show where the domain is actually pointed to.
 ServerList=Servers   #copy the userdomains file to this path
 ListLength=$(ls -1 $ServerList | grep -v "'" | wc -l)
-echo " Domain,Original,Duplicates"
+echo " Domain,Original,Duplicates" > Output
 for i in `ls -1 $ServerList | grep -v "'"`
   do
     awk '{print $1}' $ServerList/$i | rev |cut -c2-| rev > $ServerList/$i.tmp   #Cut only the domain names from userdomains
@@ -15,12 +15,12 @@ for i in `ls -1 $ServerList | grep -v "'" | grep tmp`
 for j in `cat $ServerList/$i`
   do
     DUP=$(grep -w common *.tmp | grep -v Server1.tmp | cut -f1 -d ":")
-    
-OP=$(grep -w $j $ServerList/*.tmp | grep -v $i)
 
-if  $OP ;
+#OP=$(grep -w $j $ServerList/*.tmp | grep -v $i)
+
+if  $DUP | wc -l -gt 0 ;
   then
-    echo -e " $j , $(dig +short -x `dig  +short $j`) , $OP"
+    echo -e " $j , $(dig +short -x `dig  +short $j`) , $DUP" >> Output
 
     sed '/\<$j\>/d'
 done
