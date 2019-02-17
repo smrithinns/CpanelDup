d7b3168bd02685cb16a4996e697398d7b719ecc7
smrithinns <smrithinns@yahoo.co.in>
2019-02-17 08:18:28 +0530

    Almost

--- DuplDom.sh
+++ DuplDom.sh
@@ -3,32 +3,57 @@
 #Script to read list of domains in multiple cpanel server and find duplicates and show where the domain is actually pointed to.
 ServerList=Servers   #copy the userdomains file to this path
 ListLength=$(ls -1 $ServerList | grep -v "'" | wc -l)
-echo " Domain,Original,Duplicates" > Output
+echo "Domain  Original  Duplicates" # > Output
+echo ""  > Common.txt
 for i in `ls -1 $ServerList | grep -v "'"`
   do
     awk '{print $1}' $ServerList/$i | rev |cut -c2-| rev > $ServerList/$i.tmp   #Cut only the domain names from userdomains
   done
 
 #
-for i in `ls -1 $ServerList | grep -v "'" | grep tmp`
+for i in `ls -1 $ServerList | grep -v "'" | grep tmp` #loop the serverlist
  do
-for j in `cat $ServerList/$i`
+   #echo $i
+
+for j in `cat $ServerList/$i`  #loop the domains
   do
-    DUP=$(grep -w common *.tmp | grep -v Server1.tmp | cut -f1 -d ":")
+    #echo $j
+DUP=$(grep -w $j $ServerList/*.tmp | cut -f1 -d ":")
+DUPWC=$(grep -w $j $ServerList/*.tmp | grep -v $i | cut -f1 -d ":"| wc -l) #check if found in multiple files
 
-#OP=$(grep -w $j $ServerList/*.tmp | grep -v $i)
+GRP=$(grep -c $j Common.txt)
 
-if  $DUP | wc -l -gt 0 ;
-  then
-    echo -e " $j , $(dig +short -x `dig  +short $j`) , $DUP" >> Output
+if [ $GRP -gt 0 ];
+then
+  :
+else
 
-    sed '/\<$j\>/d'
+while [ $DUPWC -gt 0 ]
+do
+
+ echo "$j" >> Common.txt
+
+IP=$(dig  +short $j)
+
+RDNS=$(dig -x $IP +short | head -1)
+
+
+echo $j $RDNS $DUP
+
+break
+
 done
+
+#host $IP | awk '{print $5}'
+
+fi
+
 done
+  done
 
 
 
 
 
-
-  #rm -f $ServerList/*.tmp
+rm -f $ServerList/*.tmp
+#rm -f  Common.txt
